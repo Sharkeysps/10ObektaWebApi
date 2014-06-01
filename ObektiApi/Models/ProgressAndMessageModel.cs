1de3789d47c3c5fb4fc6dd3efd64344e5905daf8
Sharkeysps <harda1@abv.bg>
2014-06-01 23:27:27 +0300

    Bug fixes

---
+++ ObektiApi/Models/ProgressAndMessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ObektiApi.Models
+{
+    public class ProgressAndMessageModel
+    {
+        public String Message { get; set; }
+        public int ProgressNumber { get; set; }
+    }
+}
